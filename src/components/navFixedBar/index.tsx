8548c3a8485a101c25d61b3b6e63f1db9fcfad0c
Ruan Nascimento <154020857+Ruan-nascimento@users.noreply.github.com>
2024-10-31 13:52:17 -0300

    update: navbar

--- src/components/navFixedBar/index.tsx
+++ src/components/navFixedBar/index.tsx
@@ -3,7 +3,7 @@ import { Patterns } from "../patterns"
 
 export const NavFixedBar = () => {
   return(
-    <nav className="w-5pc bg-black/80 backdrop-blur-lg backdrop-brightness-50 shadow-white/5 shadow-xl absolute bottom-3 left-2-5pc rounded-full flex justify-between overflow-hidden p-1">
+    <nav className="w-5pc bg-black/80 backdrop-blur-lg backdrop-brightness-50 shadow-white/5 shadow-xl absolute bottom-8 left-2-5pc rounded-full flex justify-between overflow-hidden p-1">
 
       <Patterns.button.button className="max-w-full min-w-2 w-33pc min-h-2 max-h-full h-20 bg-transparent hover:bg-RoxoPrincipal/15 active:opacity-80 in-moment">
         <Patterns.button.iconFa icon={faArrowsUpDown} className="text-RoxoSecundário"/>
